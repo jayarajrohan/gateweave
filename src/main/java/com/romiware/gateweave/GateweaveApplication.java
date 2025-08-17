f2498453bfb20a684848e71f4e4fb473ec95f44c
jayarajrohan <iaminbest@gmail.com>
2025-08-17 23:43:24 +0530

    initial commit

---
+++ src/main/java/com/romiware/gateweave/GateweaveApplication.java
@@ -0,0 +1,13 @@
+package com.romiware.gateweave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateweaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateweaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romiware/gateweave/GateweaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romiware.gateweave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateweaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
